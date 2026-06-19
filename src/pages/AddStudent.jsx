f9d7cf45f0da9348a220df31117881548bd0552b
Shweta Kumbhar <kumbharshweta636@gmail.com>
2026-06-19 13:03:36 +0530

    Setup project structure and routing

---
+++ src/pages/AddStudent.jsx
@@ -0,0 +1,5 @@
+function AddStudent() {
+  return <h2>Add Student Page</h2>;
+}
+
+export default AddStudent;
---
+++ src/pages/StudentList.jsx
@@ -0,0 +1,5 @@
+function StudentList() {
+  return <h2>Student List Page</h2>;
+}
+
+export default StudentList;
